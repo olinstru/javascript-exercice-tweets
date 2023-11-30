3df3dbfaaab4d5bab213f60efe5208b82be8fe6e
Olivia LINSTRUMELLE <olinstru@gmail.com>
2023-11-30 13:48:52 +0100

    partie 1

--- index.js
+++ index.js
@@ -30,3 +30,31 @@ const tweets = [
     likes: 20
   }
 ]
+
+// Je créé mon élément <li> pour afficher le tweet
+const li = document.createElement("li")
+li.classList.add("tweet")
+
+// Je créé un élément <p> pour stocker "content"
+const p_content = document.createElement("p")
+p_content.classList.add("content")
+p_content.textContent = tweets[0].content
+li.appendChild(p_content)
+
+// Je créé un élément <p> pour stocker "likes"
+const p_likes = document.createElement("p")
+p_likes.classList.add("likes")
+p_likes.textContent = tweets[0].likes
+li.appendChild(p_likes)
+
+// Je créé un élément <p> pour stocker "author"
+const p_author = document.createElement("p")
+p_author.classList.add("author")
+p_author.textContent = tweets[0].author
+li.appendChild(p_author)
+
+// Je récupère l'élément "tweets-list" dans le HTML
+const ul = document.getElementById("tweets-list")
+
+// Je lie mon élément <li> à la tweets-list dans le HTML
+ul.appendChild(li)
